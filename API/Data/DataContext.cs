160cbc75a3f1a53937b491ac778ca62b07f02ea5
PatMasterson <prmasterson@yahoo.com>
2023-01-30 14:30:58 -0500

    End of section 2

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.EntityFrameworkCore;
+using API.Entities;
+
+namespace API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+        
+    }
+}
